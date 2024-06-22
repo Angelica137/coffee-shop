df4aafe72a1bc5a967a79ba4f05e56724114bd27
Angelica <Angelica137@users.noreply.github.com>
2024-06-22 15:17:21 +0100

    get drinks, green

--- frontend/src/app/pages/drink-menu/drink-menu.page.ts
+++ frontend/src/app/pages/drink-menu/drink-menu.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DrinksService } from '../../services/drinks.service';
+import { DrinksService, Drink } from '../../services/drinks.service';
 import { ModalController } from '@ionic/angular';
 import { DrinkFormComponent } from './drink-form/drink-form.component';
 import { AuthService } from 'src/app/services/auth.service';
@@ -13,25 +13,41 @@ export class DrinkMenuPage implements OnInit {
   Object = Object;
 
   constructor(
-    private auth: AuthService,
+    public auth: AuthService,  // Changed to public so it can be accessed in the template
     private modalCtrl: ModalController,
     public drinks: DrinksService
-    ) { }
+  ) { }
 
   ngOnInit() {
-    this.drinks.getDrinks();
+    this.loadDrinks();
   }
 
-  async openForm(activedrink: any = null) {
-    if (!this.auth.can('get:drinks-detail')) {
-      return;
-    }
+  loadDrinks() {
+    console.log('Loading drinks...');
+    this.drinks.getDrinks().subscribe(
+      (response: any) => {
+        console.log('Drinks response:', response);
+        this.drinks.drinksToItems(response.drinks);
+        console.log('Updated drinks:', this.drinks.items);
+      },
+      (error) => {
+        console.error('Error fetching drinks', error);
+      }
+    );
+  }
 
+  async openForm(activedrink: any = null) {  // Changed Drink to any to match your existing code
+    // Remove permission checks for viewing drinks, as it should be public
     const modal = await this.modalCtrl.create({
       component: DrinkFormComponent,
       componentProps: { drink: activedrink, isNew: !activedrink }
     });
 
     modal.present();
+
+    const { data } = await modal.onWillDismiss();
+    if (data) {
+      this.loadDrinks();
+    }
   }
 }
